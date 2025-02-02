c42c1a01fcd60262cf8a7497fe4b64a67f7f6a31
n232not <nihanth.pinnaka@gmail.com>
2025-02-02 16:28:17 -0500

    .gitignore changes

--- KhanAcademyClone/settings.py
+++ KhanAcademyClone/settings.py
@@ -90,11 +90,10 @@ import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 if 'DATABASE_URL' in os.environ:
