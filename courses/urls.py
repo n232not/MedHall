90fad965b68d02ce46d2b8eda1b61767078e8f75
n232not <nihanth.pinnaka@gmail.com>
2025-02-14 02:09:28 -0500

    Fixed urls

--- courses/urls.py
+++ courses/urls.py
@@ -5,9 +5,9 @@ from django.conf import settings
 from django.urls import path
 
 urlpatterns = [
-    path('/<int:course_id>/', views.course_detail, name='course_detail'),
-    path('/<int:course_id>/unit/<int:unit_id>/', views.unit_detail, name='unit_detail'),
-    path('/<int:course_id>/unit/<int:unit_id>/lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
+    path('<int:course_id>/', views.course_detail, name='course_detail'),
+    path('<int:course_id>/unit/<int:unit_id>/', views.unit_detail, name='unit_detail'),
+    path('<int:course_id>/unit/<int:unit_id>/lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
     path('ckeditor5/upload/', views.ckeditor_image_upload, name='ck_editor_5_upload_file'),  # Add this line
     path('', views.course_list, name='course_list'),
 ]
